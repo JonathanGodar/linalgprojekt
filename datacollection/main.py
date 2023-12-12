fafd7d6d0d5dd95c3165fcc26ad8f0c758a6a498
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2023-12-12 17:13:23 +0100

    Cleanup and commenting of code

--- datacollection/main.py
+++ datacollection/main.py
@@ -1,46 +1,45 @@
 from dotenv import load_dotenv
-from constants import API_SECRET_ENV_NAME, API_CLIENT_ID_ENV_NAME
-import base64
+from constants import API_SECRET_ENV_NAME, API_CLIENT_ID_ENV_NAME,REDDIT_API_URL_ENV_NAME
 import time
-import praw
 import json
-# from psaw import PushshiftAPI
 import os
 import logging
 from urllib import request
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-for logger_name in ("praw", "prawcore"):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+def get_comments_from_subreddit(subreddit: str):
+    '''
+    Retrieves comments from a subreddit.
+    Expects the environment variables in ./constants.py to be set.
+    '''
 
-def get_comments_from_subreddit(subreddit: str, hello):
-    api = os.environ['REDDIT_API_URL']
+    subreddit = subreddit.lower()
+
+	# Load env variables
+    api = os.environ[REDDIT_API_URL_ENV_NAME]
     client_id = os.environ[API_CLIENT_ID_ENV_NAME]
     client_secret = os.environ[API_SECRET_ENV_NAME]
 
+	# Dictionary containing all scraped comments(value) and their id(key) - or name as reddit calls them
     comments_dict = {}
+    
+
+	# Is the "pointer" that controls the pagination.
     after = ''
+    # Go through the pagination and attempt to retrieve one page for each iteration
     while True:
-        time.sleep(3)
+        time.sleep(3) # Sleep for 3 secs to to avoid reddit API ratelimiting errors
         after_param = f'&after={after}' if after != '' else ''
+        
+        # Construct the request for comments from the specified subreddit
         req = request.Request(f'{api}/r/{subreddit}/comments/.json?limit=100&raw_json=1{after_param}')
         req.add_header("Authorization", f'Basic {client_id}:{client_secret}')
         req.add_header("User-agent", "Subreddit comment scraping bot")
 
-        # try:
-        print("Sending request")
+		# Send and parse the request
         res = request.urlopen(req)
-        # except:
-        #     print("Rate limit... sleeping")
-        #     time.sleep(30)
-        #     continue
-            # get_comments_from_subreddit(subreddit, before, True)
-
         j = json.loads(res.read())
 
+        # Add the found comments tho the comment dict
         for comment in j['data']['children']:
             comment = comment['data']
 
@@ -49,49 +48,18 @@ def get_comments_from_subreddit(subreddit: str, hello):
         print(len(comments_dict))
         after = j['data']['after']
 
-        # print(j['data']['children'][0]['data'].keys())
         if after == '':
             print('No more comments')
             return
         
+        # Save the comments. Note this will create a separate file for each itteration of the loop 
         with open(f'datacollection/scraped/{subreddit}_{len(comments_dict)}.txt', 'w', encoding='utf-8') as f:
             f.writelines(json.dumps(comments_dict))
 
-        # time.sleep(3)
-
-
-
-
 def main():
+    """ Scrape comments from a subreddit """
     load_dotenv()
-    get_comments_from_subreddit('techsupport', True)
-    # api = PushshiftAPI()
-
-    # api.search_comments(subreddit="relationship_advice")
-
-    # client_id = os.environ[API_CLIENT_ID_ENV_NAME]
-    # client_secret = os.environ[API_SECRET_ENV_NAME]
-    # reddit = praw.Reddit(client_id= client_id, client_secret=client_secret, user_agent="testscript")
-    
-    # subreddit = reddit.subreddit("relationship_advice")
-    
-    # comment_count = 0
-
-    # # comments = subreddit.comments.replace_more(limit=2048)
-    # comments = subreddit.comments(limit=None)
-
-    # # print(type(comments))
-    # for comment in comments:
-    #     # print(comment.body)
-    #     comment_count += 1
-    #     # time.sleep(1)
-    #     # print("Comment count" omment_count)
-    # print("Got: ", comment_count)
-    
-    # # print(reddit.read_only)
-
-    # for submission in reddit.subreddit("relationship_advice").top():
-    #     print(submission.title)
+    get_comments_from_subreddit('Rhetoric')
 
 if __name__ == "__main__":
     main()
